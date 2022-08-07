f120857fcc357892edc11e6857fa051f2356db1f
Boris Kuznetsov <boris.kuznetsov@latoken.com>
2022-08-07 23:49:09 +0300

    QueryString

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 pub enum Method {
     Get,
     Delete,
@@ -9,3 +11,25 @@ pub enum Method {
     Trace,
     Patch,
 }
+
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::Get),
+            "DELETE" => Ok(Self::Delete),
+            "POST" => Ok(Self::Post),
+            "PUT" => Ok(Self::Put),
+            "HEAD" => Ok(Self::Head),
+            "CONNECT" => Ok(Self::Connect),
+            "OPTIONS" => Ok(Self::Options),
+            "TRACE" => Ok(Self::Trace),
+            "PATCH" => Ok(Self::Patch),
+            _ => Err(String::from("error")),
+        };
+        todo!()
+    }
+}
+
+pub struct MethodError;
